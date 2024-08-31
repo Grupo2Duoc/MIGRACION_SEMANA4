6a91b36c28994da0947485e2002aaaf35aca8130
Grupo2Duoc <riv.uribe@duocuc.cl>
2024-08-31 00:08:23 -0400

    update

--- semana4/core/urls.py
+++ semana4/core/urls.py
@@ -9,36 +9,17 @@ from .views import index, callofduty, registro, accion, rpg1, deportes, familiar
 urlpatterns=[
     path('', index, name="index"),
     path('callofduty/', callofduty, name="callofduty"),
-
-    path('registro/', registro, name="registro"),
-    # otras rutas
-
+    path('registro/', registro, name="registro"),   
     path('rpg1/', rpg1, name="rpg1"),
-
     path('accion/', accion, name="accion"),
-
     path('familiar/', familiar, name="familiar"),
-
     path('deportes/', deportes, name="deportes"),
-
     path('horror/', horror, name="horror"),
-
     path('godofwar/', godofwar, name="godofwar"),
-
     path('fallguys/', fallguys, name="fallguys"),
-
     path('baldursgate/', baldursgate, name="baldursgate"),
+    path('perfil/', perfil, name="perfil"),
+    path('registro/', recuperar_contrasena, name="recuperar_contrasena"),
 
-        path('perfil/', perfil, name="perfil"),
-
-    path('recuperar_contrasena/', perfil, name="recuperar_contrasena"),
-
-
-
-
-
-
-
-
-
+ 
 ]
